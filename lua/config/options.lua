db6fd4162d23aa729f3fffa2c21c29c7a3253a12
Jake Broughton <46027440+JakeBroughton@users.noreply.github.com>
2026-03-05 11:22:23 +0000

    feat: add env file support to config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Load .env file from nvim config directory
+local env_file = vim.fn.stdpath("config") .. "/.env"
+if vim.fn.filereadable(env_file) == 1 then
+  for line in io.lines(env_file) do
+    local key, value = line:match("^([%w_]+)=(.*)")
+    if key and value then
+      vim.env[key] = value
+    end
+  end
+end
